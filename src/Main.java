f374e064711f08287f252d72b61112adaa04ecc8
vadim <vAdimkanivez@gmail.com>
2025-05-03 08:55:47 +0300

    masntr

--- src/Main.java
+++ src/Main.java
@@ -12,50 +12,40 @@ public class Main {
             System.out.println("Сумма трат за месяц составила " + sum + " P");
 
             System.out.println();
-            System.out.println("------");
+            System.out.println("------");}
 
-        }
-        System.out.println("Task 2");
-        int minPayment = monthPayments[0];
-        int maxPayment = monthPayments[0];
-
-
-        for (int payment : monthPayments) {
-            if (payment > maxPayment)
+        System.out.println( " Task 2");
+        int mintPayment = monthPayments[0];
+        int maxPayment = monthPayments [0];
+        for (int payment : monthPayments){
+            if (payment> maxPayment){
                 maxPayment = payment;
-
-
-
-            if (  payment < minPayment) {
-                minPayment = payment;
             }
-
-
-            System.out.println("Минимальная сумма трат на неделю составила " + minPayment + "P" +
-                    " Максимальная сумма трат на неделю составила " + maxPayment + " P");
-
-
-            System.out.println();
-            System.out.println("------");
-
-            System.out.println("Task 3");
-
-            double averageMonthPayment = (double) sum / monthPayments.length;
-
-            System.out.println(" Средняя сумма трат за месяц составила " + averageMonthPayment + " P");
-
-
-            System.out.println();
-            System.out.println("------");
-
-            System.out.println("Task 4");
-
-            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-            for (int e = reverseFullName.length - 1; e >= 0; e--) {
-                System.out.print(reverseFullName[e]);
+            if (payment < mintPayment) {
+                mintPayment = payment;
             }
+        }
+        System.out.println(" Минимальная сумма трат за неделю составила "+ mintPayment +" P "+
+                " Максимальная сумма трат за неделю составила " + maxPayment+ " P");
+
+        System.out.println();
+        System.out.println("------");
+
+        System.out.println(" Task 3");
+
+        double  averageMonthPayment = (double) sum/monthPayments.length ;
+        System.out.println(" Средняя сумма трат за месяц составила " + averageMonthPayment + " P");
+
+        System.out.println(" Task 4");
 
 
-            System.out.println();
-            System.out.println("------");
-        }}}
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+
+        for (int i = reverseFullName.length -1;i>=0;i--){
+            System.out.print(reverseFullName[i]);
+        }
+        System.out.println();
+        System.out.println("------");
+
+
+    }}
